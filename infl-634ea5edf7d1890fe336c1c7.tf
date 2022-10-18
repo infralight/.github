0014aab739ababc527f04a7bfd3611cc0029f9ff
sefi-infralight <71797160+sefi-infralight@users.noreply.github.com>
2022-10-18 16:11:11 +0300

    Codification: aws_ssm_parameter

---
+++ infl-634ea5edf7d1890fe336c1c7.tf
@@ -0,0 +1,7 @@
+resource "aws_ssm_parameter" "stag-iacathon-crawler-producer-chunks-06d" {
+  arn   = "arn:aws:ssm:eu-west-1:824784664836:parameter/stag-iacathon-crawler-producer-chunks"
+  name  = "stag-iacathon-crawler-producer-chunks"
+  type  = "String"
+  value = "REDACTED-BY-FIREFLY"
+}
+
