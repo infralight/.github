7f22444cab6b23e2156e81f20ef9b53b49c06cba
ahmdsalahme <ahmdsalah.me@gmail.com>
2025-03-19 23:23:52 +0400

    Auto Commit - 202503192323

--- .github/actions/calc-cache-vars/golang.sh
+++ .github/actions/calc-cache-vars/golang.sh
@@ -21,6 +21,7 @@ if [ "$target" != "*" ]; then
     echo "cache-key-any=golang-v$go_version-$OS_RUNNER_KEY-$ARCHITECTURE-$VERB-$APP_NAME-checksum-" >> "$GITHUB_OUTPUT"
     echo "cache-key-any2=golang-v$go_version-$OS_RUNNER_KEY-$ARCHITECTURE-$VERB-" >> "$GITHUB_OUTPUT"
 else
+    ls **/go.sum
     checksum=$(find . -name go.sum -exec sha256sum {} \; | head -n 1 | awk '{print $1}' | cut -c 1-6)
     echo "cache-key=golang-v$go_version-$OS_RUNNER_KEY-$ARCHITECTURE-$VERB-checksum-$checksum"
     echo "cache-key=golang-v$go_version-$OS_RUNNER_KEY-$ARCHITECTURE-$VERB-checksum-$checksum" >> "$GITHUB_OUTPUT"
