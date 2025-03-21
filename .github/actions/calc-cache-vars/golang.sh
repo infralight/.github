af0c42d2d9f9e89852f9edd07e2a1b7c1c39bd34
ahmdsalahme <ahmdsalah.me@gmail.com>
2025-03-21 17:07:51 +0400

    Auto Commit - 202503211707

--- .github/actions/calc-cache-vars/golang.sh
+++ .github/actions/calc-cache-vars/golang.sh
@@ -3,8 +3,6 @@
 # Export Target Cache Type (e.g. "app-name" or "MONO_REPO")
 target=$(find . -name go.mod | wc -l | awk -v app="$APP_NAME" '{print ($1>1)?app:"MONO_REPO"}')
 echo "target=$([ "$target" = "MONO_REPO" ] && echo "*" || echo "$target")" >> "$GITHUB_OUTPUT"
-echo "$target"
-echo "target=$([ "$target" = "MONO_REPO" ] && echo "*" || echo "$target")" 
 
 # Export Go Cache Paths
 echo "go-build=/home/runner/.cache/go-build" >> "$GITHUB_OUTPUT"
